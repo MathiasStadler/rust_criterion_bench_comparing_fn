221c8bca75848fa8a276ae9e55b07491bf29dc98
dudoof <email@example.com>
2023-07-11 10:08:34 +0200

    error fix

--- src/lib.rs
+++ src/lib.rs
@@ -7,22 +7,17 @@ pub fn fibonacci_slow(n: u64) -> u64 {
     }
 }
 
-pub fn fibonacci_slow_result<T,E>(n: u64, my_err: &'static str) -> Result<u64, &'static str> {
-    
-
+pub fn fibonacci_slow_result<T, E>(n: u64, my_err: &'static str) -> Result<u64, &'static str> {
     match n {
         0 => Err("Fibonacci start with at 1"),
         1 => Ok(1),
-        _ => { fibonacci_slow_result::<u64,&'static str>(n - 1,my_err );
+        _ => { let _ = fibonacci_slow_result::<u64, &'static str>(n - 1, my_err);
             // { fibonacci_slow_result(n - 1,my_err + fibonacci_slow_result(n - 2,my_err));
-        
-        
+
             Ok(n)
         }
-        }
-        
     }
-
+}
 
 #[inline]
 pub fn fibonacci_fast(n: u64) -> u64 {
@@ -42,10 +37,8 @@ pub fn fibonacci_fast(n: u64) -> u64 {
     }
 }
 
-
 #[inline]
-pub fn fibonacci_fast_result<T,E>(n: u64) -> Result<u64, &'static str> {
-    
+pub fn fibonacci_fast_result<T, E>(n: u64) -> Result<u64, &'static str> {
     let mut a: u64 = 0;
     let mut b: u64 = 1;
 
